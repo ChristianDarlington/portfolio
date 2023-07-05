f8a7d22cfd283261bb82707c5a3db42bd1ed79f8
Christian <christiandarlington0@gmail.com>
2023-07-05 12:49:29 -0400

    added resume button and fixed hover buttons

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,3 +1,5 @@
+import Resume from '../Resume.pdf'
+
 const About = () => {
   return (
     <div id='about' className='w-full h-screen  text-gray-800'>
@@ -15,12 +17,15 @@ const About = () => {
               <p>Hi. I'm Christian Darlington, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p className="text-[#001b5e]"> I'm a junior software developer with experience in React.js. I have a strong passion for creating intuitive and efficient user
+              <p className="text-[#001b5e]"> I'm a software developer with experience in React.js and Next.js. I have a strong passion for creating intuitive and efficient user
               interfaces. My expertise lies in developing scalable and maintainable web applications using React.js and its ecosystem. With a solid
               understanding of JavaScript, HTML, and CSS, I am adept at building responsive and interactive UI components. I am eager to contribute to
               dynamic development teams and continue expanding my skills in front-end development.</p>  
             </div>
           </div>
+          <a href={Resume} target='_blank' rel='noreferrer'>
+              <p className="p-4 rounded-lg bg-[#001b5e] text-white font-bold cursor-pointer mt-8">My Resume</p>
+            </a>
       </div>
     </div>
   );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,13 +2,13 @@ import React from 'react'
 import BettingApp from '../Assets/BettingApp.png'
 import TrendingMovie from '../Assets/TrendingMovie.png'
 import ScoreApp from '../Assets/ScoreApp.png'
-import ScreenShotOne from '../Assets/Screen Shot 2023-05-22 at 1.23.53 PM.png'
+import Foresight from '../Assets/Foresight.png'
 
 
 
 const Content = () => {
   return (
-    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-20">
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-24">
       <h1 className="text-4xl font-bold text-center text-[#001b5e] tracking-widest">Projects</h1>
       <p className="text-center py-8 text-lg text-[#001b5e]">
       In my project section, I have highlighted my experience with React.js, 
@@ -17,52 +17,73 @@ const Content = () => {
       interfaces while effectively utilizing React's features and best practices.
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-grey-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
-          <img src={BettingApp} alt="" className="rounded-xl group-hover:opacity-10" />
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-700 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+          <img src={Foresight} alt="" className="rounded-xl group-hover:opacity-10" />
           <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <h3 className="text-2xl font-bold text-white tracking-wider text-center">Baller Bet</h3>
-
+            <h3 className="text-2xl font-bold text-white tracking-wider text-center">SkillForesight</h3>
+        
             <p className="pb-4 pt-2 text-white text-center">React Js</p>
 
-            <a href="https://github.com/ChristianDarlington/sports-betting" target='_blank' rel='noreferrer'>
-              <p className="text-center p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
+        <div className='flex space-x-4 pl-4'>
+            <a href="https://admirable-pothos-436a51.netlify.app/" target='_blank' rel='noreferrer'>
+              <p className=" p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Demo</p>
             </a>
+            <a href="https://github.com/ChristianDarlington/career-finder" target='_blank' rel='noreferrer'>
+              <p className="p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
+            </a>
+        </div>
+
           </div>
         </div>
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-grey-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-700 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
           <img src={TrendingMovie} alt="" className="rounded-xl group-hover:opacity-10" />
           <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
             <h3 className="text-2xl font-bold text-white tracking-wider text-center">Daily Entertainment</h3>
 
             <p className="pb-4 pt-2 text-white text-center">React Js</p>
 
-            <a href="https://github.com/ChristianDarlington/trending-movies-app" target='_blank' rel='noreferrer'>
-              <p className="text-center p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
+            <div className='flex space-x-4 items-center justify-center'>
+            <a href="https://resplendent-cascaron-42edb6.netlify.app/" target='_blank' rel='noreferrer'>
+              <p className=" p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Demo</p>
             </a>
+            <a href="https://github.com/ChristianDarlington/trending-movies-app" target='_blank' rel='noreferrer'>
+              <p className="p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
+            </a>
+        </div>
           </div>
         </div>
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-grey-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-700 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+          <img src={BettingApp} alt="" className="rounded-xl group-hover:opacity-10" />
+          <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h3 className="text-2xl font-bold text-white tracking-wider text-center">Baller Bet</h3>
+
+            <p className="pb-4 pt-2 text-white text-center">React Js</p>
+
+            <div className='flex space-x-4'>
+            <a href="https://moonlit-malasada-dec0fa.netlify.app/" target='_blank' rel='noreferrer'>
+              <p className="p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Demo</p>
+            </a>
+            <a href="https://github.com/ChristianDarlington/sports-betting" target='_blank' rel='noreferrer'>
+              <p className=" p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
+            </a>
+        </div>
+          </div>
+        </div>
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-700 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
           <img src={ScoreApp} alt="" className="rounded-xl group-hover:opacity-10" />
           <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <h3 className="text-2xl font-bold text-white tracking-wider text-center">Score App</h3>
+            <h3 className="text-2xl font-bold text-white tracking-wider text-center">Scoreboard</h3>
 
             <p className="pb-4 pt-2 text-white text-center">React Js</p>
 
+            <div className='flex space-x-4'>
+            <a href="https://scoreboardpoints.netlify.app/" target='_blank' rel='noreferrer'>
+              <p className="p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Demo</p>
+            </a>
             <a href="https://github.com/ChristianDarlington/react-score-app" target='_blank' rel='noreferrer'>
-              <p className="text-center p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
+              <p className=" p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
             </a>
-          </div>
         </div>
-        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-grey-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
-          <img src={ScreenShotOne} alt="" className="rounded-xl group-hover:opacity-10" />
-          <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <h3 className="text-2xl font-bold text-white tracking-wider text-center">Grammerhub Projects</h3>
-
-            <p className="pb-4 pt-2 text-white text-center">React Js</p>
-
-            <a href="https://github.com/grammerjam/te-main" target='_blank' rel='noreferrer'>
-              <p className="text-center p-2 rounded-lg bg-white text-gray-800 font-bold cursor-pointer">Github</p>
-            </a>
           </div>
         </div>
         
